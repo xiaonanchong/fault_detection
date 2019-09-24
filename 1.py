9c41acbe28d9750dbf0b6a15f5be2a9bdc3921be
xiaonanchong <xnchong@outlook.com>
2019-09-25 02:25:44 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -7,8 +7,8 @@ from keras.models import Sequential
 from keras.layers import Dense, Activation
 
 # generate training data
-N = 1000
-L = 2000
+N = 3 #1000
+L = 20 #2000
 
 x_train = []
 y_train = []
@@ -16,6 +16,7 @@ for i in range(N):
 	r1 = random.gauss(0,0.5)
 	r2 = random.gauss(0,0.5)
 	x1 = [math.sin(i*r1 + r2) for i in range(L)]
+	print(x1)
 	
 	noise = [random.uniform(0,1) for i in range(L)]
 	x2 = x1 + noise
@@ -28,7 +29,7 @@ for i in range(N):
 print(x_train)
 
 model = Sequential()
-model.add(Dense(512, activation='relu', input_dim=2000))
+model.add(Dense(512, activation='relu', input_dim=L))
 model.add(Dense(64, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
 model.compile(optimizer='rmsprop',
