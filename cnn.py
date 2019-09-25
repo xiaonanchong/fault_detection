6a200c982a34220699d9102ab5dfd3b6f9a5f016
xiaonanchong <xnchong@outlook.com>
2019-09-25 18:33:06 +0800

    Add files via upload

---
+++ cnn.py
@@ -0,0 +1,43 @@
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Activation
+import numpy as np
+import numpy.matlib
+import random
+import matplotlib.pyplot as plt
+
+L = 2000
+
+model = Sequential()
+model.add(Dense(512, activation='relu', input_dim=L))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+model.compile(optimizer='rmsprop',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+x_trian = random.shuffle(x_train)
+history = model.fit(x_train, y_train, validation_split=0.33, epochs=100, batch_size=32)
+
+
+# list all data in history
+print(history.history.keys())
+# summarize history for accuracy
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+#plt.show()
+plt.savefig("train_acc.png")
+
+# summarize history for loss
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+#plt.show()
+plt.savefig("train_loss.png")
