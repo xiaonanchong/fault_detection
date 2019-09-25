acb1c2f665f14a1feb6ebe9187284a7f1c8c7214
xiaonanchong <xnchong@outlook.com>
2019-09-25 18:00:43 +0800

    Update generate_data.py

--- generate_data.py
+++ generate_data.py
@@ -2,35 +2,29 @@ import numpy as np
 import math
 
 # generate 2*N training data
-N = 2
+N = 10000
 L = 2000
+l = 200
 
-
-s1 = np.random.normal(10, 20, N) 
-s2 = np.random.normal(5, 20, N)
-s3 = np.random.normal(20, 2, N)
-
-s = s1 + s2 + s3
-
-print(s)
-
-'''
-x_train = np.empty((N*2, L))
-y_train = np.empty((N*2, 1))
+x = np.empty((N*2, L))
+y = np.empty((N*2, 1))
 for i in range(N):
+	s1 = np.random.normal(10, 20, L) 
+	s2 = np.random.normal(5, 20, L)
+	s3 = np.random.normal(20, 2, L)
+	f1 = np.random.normal(4, 20, l)
 
-	s1 = math.sqrt(20) * np.matlib.randn(L) + 10
-	s2 = math.sqrt(20) * np.matlib.randn(L) + 5
-	s3 = math.sqrt(2) * np.matlib.randn(L) + 20
-	print(x1)
+	s = s1 + s2 + s3
 	
-	sigma = 2
-	mu = 0
-	x2 = sigma * np.matlib.randn(L) + mu
-	print(x2)
+	f = s
+	index = random.randint(0, L-l)
+	for j in range(l):
+		f[j+index] += f1[index]
 	
-	x_train[2*i] = x1
-	x_train[2*i+1] = x2
-	y_train[2*i] = 1
-	y_train[2*i+1] = 0
-'''
+	x[2*i] = s
+	x[2*i+1] = f
+	y[2*i] = 1
+	y[2*i+1] = 0
+
+np.save('x.npy', x)
+np.save('y.npy', y)
