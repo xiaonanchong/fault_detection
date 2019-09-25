4d5e00706ea2eeb1c13969e140d3f640f3cacf1b
xiaonanchong <xnchong@outlook.com>
2019-09-25 08:38:38 +0800

    Add files via upload

---
+++ exp1.py
@@ -0,0 +1,36 @@
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Activation
+import numpy as np
+
+# generate 2*N training data
+N = 1000
+L = 2000
+
+x_train = np.empty((N*2, L))
+y_train = np.empty((N*2, 1))
+for i in range(N):
+	sigma = 1
+	mu = 0
+	x1 = sigma * np.matlib.randn(L) + mu
+	print(x1)
+	
+	sigma = 2
+	mu = 0
+	x2 = sigma * np.matlib.randn(L) + mu
+	print(x2)
+	
+	x_train[2*i] = x1
+	x_train[2*i+1] = x2
+	y_train[2*i] = 1
+	y_train[2*i+1] = 0
+
+model = Sequential()
+model.add(Dense(512, activation='relu', input_dim=L))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+model.compile(optimizer='rmsprop',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=100, batch_size=32)
