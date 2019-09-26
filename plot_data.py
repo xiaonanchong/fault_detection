8fd945ca0c68c0845242b6f6a53d9c2d217e1136
xiaonanchong <xnchong@outlook.com>
2019-09-26 23:30:58 +0800

    Update plot_data.py

--- plot_data.py
+++ plot_data.py
@@ -13,12 +13,14 @@ y = np.load(args.y_path+'.npy')
 L = 2000
 l = 200
 s1 = np.random.normal(1.0, 2.0, L) 
-f1 = np.random.normal(0.4, 2.0, l)
+f0 = np.random.normal(0.4, 2.0, l)
+
 s = s1
-f = s
+f1 = np.zeros(L)
 index = np.random.randint(0, L-l)
 for j in range(l):
-	f[j+index] += f1[j]
+	f1[j+index] += f0[j]
+f = s1+f1
 
 print('check:',np.array_equal(f,s))
 	
@@ -29,6 +31,8 @@ plt.subplot(2,2,2)
 plt.plot(f, 'r.')
 plt.subplot(2,2,3)
 plt.plot(f, 'r.', s, 'b.', )
+plt.subplot(2,2,4)
+plt.plot(f1, 'g.')
 plt.savefig('data.png')
 
 '''
