5d5fcdc2b7f5546c0d9b5279e77d681b67a77b6d
xiaonanchong <xnchong@outlook.com>
2019-09-26 11:44:33 +0800

    Update generate_data.py

--- generate_data.py
+++ generate_data.py
@@ -15,10 +15,10 @@ l = 200
 x = np.empty((N*2, L))
 y = np.empty((N*2, 1))
 for i in range(N):
-	s1 = np.random.normal(10, 20, L) 
-	s2 = np.random.normal(5, 20, L)
-	s3 = np.random.normal(20, 2, L)
-	f1 = np.random.normal(10, 20, l)
+	s1 = np.random.normal(1.0, 2.0, L) 
+	s2 = np.random.normal(0.5, 2.0, L)
+	s3 = np.random.normal(2.0, 2.0, L)
+	f1 = np.random.normal(1.0, 2.0, l)
 
 	s = s1 #+ s2 + s3
 	
