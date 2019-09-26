b37abf13da24292a8e54b1dbb7d11a7f0f5fd7ee
xiaonanchong <xnchong@outlook.com>
2019-09-26 11:39:16 +0800

    Update generate_data.py

--- generate_data.py
+++ generate_data.py
@@ -18,7 +18,7 @@ for i in range(N):
 	s1 = np.random.normal(10, 20, L) 
 	s2 = np.random.normal(5, 20, L)
 	s3 = np.random.normal(20, 2, L)
-	f1 = np.random.normal(4, 20, l)
+	f1 = np.random.normal(10, 20, l)
 
 	s = s1 #+ s2 + s3
 	
