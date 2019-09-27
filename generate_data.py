881ecb0051c64388675b42e93e12d8a5b9be2db8
xiaonanchong <xnchong@outlook.com>
2019-09-27 10:59:23 +0800

    Update generate_data.py

--- generate_data.py
+++ generate_data.py
@@ -20,12 +20,12 @@ for i in range(N):
 	s3 = np.random.normal(2.0, 2.0, L)
 	f0 = np.random.normal(1.4, 3.0, l)
 
-	s = (s1+s2+s3)
+	s = (s1+s2+s3)/(1.0 + 0.5 + 2.0)
 	f1 = np.zeros(L)
 	index = np.random.randint(0, L-l)
 	for j in range(l):
 		f1[j+index] += f0[j]
-	f = (s1+s2+s3)+f1
+	f = ((s1+s2+s3)+f1)/((1.0 + 0.5 + 2.0)+1.4)
 
 	print('check:',np.array_equal(f,s))
 	
