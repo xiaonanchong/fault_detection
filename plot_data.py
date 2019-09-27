324fba0ba68253b6e6780bca42c39921b3ae6e96
xiaonanchong <xnchong@outlook.com>
2019-09-27 10:27:39 +0800

    Update plot_data.py

--- plot_data.py
+++ plot_data.py
@@ -1,15 +1,6 @@
 import numpy as np
 from matplotlib import pyplot as plt
-'''
-import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument("x_path", help="path to store data x", type=str)
-parser.add_argument("y_path", help="path to store data y", type=str)
-args = parser.parse_args()
 
-x = np.load(args.x_path+'.npy')
-y = np.load(args.y_path+'.npy')
-'''
 L = 2000
 l = 200
 
@@ -48,18 +39,3 @@ plt.ylim((k1,k2))
 plt.plot(f1, 'g.')
 
 plt.savefig('data.png')
-
-'''
-plt.title("nomral signal") 
-plt.plot(s)
-plt.savefig('normal_data_sample.png')
-
-plt.title("fault signal") 
-plt.plot(f)
-plt.savefig('fault_data_sample.png')
-
-plt.plot(f)
-plt.plot(s)
-plt.legend(['fault signal', 'normal signal'])
-plt.savefig('data_sample.png')
-'''
