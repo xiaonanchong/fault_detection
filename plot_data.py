4cce4aa67a95b3888efb410c7f2d213d10dad04a
xiaonanchong <xnchong@outlook.com>
2019-09-27 09:17:53 +0800

    Update plot_data.py

--- plot_data.py
+++ plot_data.py
@@ -13,18 +13,17 @@ y = np.load(args.y_path+'.npy')
 L = 2000
 l = 200
 
-
+s1 = np.random.normal(1.0, 2.0, L)
 s2 = np.random.normal(0.5, 2.0, L)
-s3 = np.random.normal(2.0, 2.0, L)
-s1 = np.random.normal(1.0, 2.0, L) 
+s3 = np.random.normal(2.0, 2.0, L) 
 f0 = np.random.normal(0.4, 2.0, l)
 
-s = (s1+s2+s3)/3.0
+s = (s1+s2+s3)
 f1 = np.zeros(L)
 index = np.random.randint(0, L-l)
 for j in range(l):
 	f1[j+index] += f0[j]
-f = (s1+s2+s3)/3.0+f1
+f = (s1+s2+s3)+f1
 
 print('check:',np.array_equal(f,s))
 	
